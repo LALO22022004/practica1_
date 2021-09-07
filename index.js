4226559e04ba7779e218d5c7cdc6ea54db964e93
LALO22022004 <89814151+LALO22022004@users.noreply.github.com>
2021-09-07 17:43:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+functiongetimc(){
+  var peso=parsefloat(document.getElementByid(peso).value);
+  var altura=parsefloat(document.getElementByid(altura).value);
+  var imc=peso/(altura*altura);
+  document.getElementByid(imc).inner html=imc;
+                    
